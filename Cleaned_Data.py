73a153e6cc09107c278ad43e0d150927682922a5
VidushiXL <vidushiur2000@gmail.com>
2023-10-13 15:55:12 +0530

    Normalization

--- Cleaned_Data.py
+++ Cleaned_Data.py
@@ -4,3 +4,15 @@ data = pd.read_csv("D:/Enored/supermarket_sales - Sheet1.csv")
 data
 missing_values_count = data.isnull().sum()
 missing_values_count
+
+# Select the sales income column
+sales_income = data['Total']
+
+# Apply min-max normalization
+min_sales_income = sales_income.min()
+max_sales_income = sales_income.max()
+normalized_sales_income = (sales_income - min_sales_income) / (max_sales_income - min_sales_income)
+
+# Create a new column for the normalized values
+data['normalized_sales_income'] = normalized_sales_income
+
