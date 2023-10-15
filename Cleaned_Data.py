ab250044644adb71ab9784703c7a16bebb36744d
VidushiXL <vidushiur2000@gmail.com>
2023-10-15 15:34:04 +0530

    Removed Duplicates

--- Cleaned_Data.py
+++ Cleaned_Data.py
@@ -16,3 +16,32 @@ normalized_sales_income = (sales_income - min_sales_income) / (max_sales_income 
 # Create a new column for the normalized values
 data['normalized_sales_income'] = normalized_sales_income
 
+import matplotlib.pyplot as plt
+# Select the normalized sales income column
+normalized_sales_income = data['normalized_sales_income']
+# Create a bar chart
+plt.bar(data.index, normalized_sales_income)
+
+# Add a title and labels for the x and y axes
+plt.title('Normalized Sales Income')
+plt.xlabel('Sales')
+plt.ylabel('Range')
+
+# Adjust the size of the chart
+plt.figure(figsize=(8, 6))
+
+# Show the chart
+plt.show()
+
+
+# Identify duplicate rows
+duplicate_rows = data[data.duplicated()]
+
+# Display duplicate rows
+print("Duplicate Rows except first occurrence:")
+print(duplicate_rows)
+
+
+
+
+
